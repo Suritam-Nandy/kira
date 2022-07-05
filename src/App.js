69a72bb7c4f388e76ffee776e2ca7ea11e96c13b
Suritam-Nandy <13suritsuritam@gmail.com>
2022-07-05 19:27:41 +0530

    ✨

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 // import "./App.css";
 
 function App() {
-  return <div className="bg-red-900 App">ok</div>;
+  return <div className="bg-red-900 App">Kira</div>;
 }
 
 export default App;
